6b54806f917876fb09196a99955c7e060044b5bd
aandjMM <102420824+aandjMM@users.noreply.github.com>
2022-03-26 10:09:56 -0700

    Update hexgame.js

--- hexgame.js
+++ hexgame.js
@@ -178,9 +178,9 @@ const enter = () => {
             isValid = false;
             incorrectWord("Missing centre letter");
         }
-        if (isValid && !dictionary.includes(word.toLowerCase())) {
+        /* if (isValid && !dictionary.includes(word.toLowerCase())) {
             isValid = false;
-            incorrectWord("Not in the fam dictionary");
+            incorrectWord("Not in the fam dictionary"); */
         }
         if (isValid && foundWordsList.includes(word)) {
             isValid = false;
